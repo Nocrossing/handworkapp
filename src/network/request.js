1c1458cc2e8f4b52a766dc1d10eb69127af82e45
Nocrossing <2409135531@qq.com>
2021-06-15 13:06:47 +0800

    resive

--- src/network/request.js
+++ src/network/request.js
@@ -4,7 +4,7 @@ export function request(config) {
 
 //1.创建axios的实例
 const instance = axios.create({
-  baseURL: '/api',
+  baseURL: './data',
   timeout:  5000
 })
 
--- vue.config.js
+++ vue.config.js
@@ -12,16 +12,21 @@ module.exports = {
             }
         }
     },
-    devServer: {
-      proxy: {  
-        '/api': {
-          target: 'http://localhost:8080', //路径指向本地主机地址及端口号
-          ws: true, 
-          changeOrigin: true,
-          pathRewrite:{
-              '^/api': '/data' //路径转发代理
-          }
+     devServer: {
+    port: 8080,
+    host: 'localhost',
+    open: true,
+    https: false,
+    proxy: {
+      '/api': {
+        target: '0.0.0.0',
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
         }
       }
     }
+
+  }
 }
